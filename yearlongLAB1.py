1160c11286d64d4864b8c350e8eecc5caf9b3214
mayam20-meet <mayam20@meet.mit.edu>
2018-12-17 12:29:34 -0500

    first commit

---
+++ yearlongLAB1.py
@@ -0,0 +1,52 @@
+#LAB1
+import turtle
+
+#for i in range(3):
+    #print("maya")
+
+#for i in range(100):
+    #print("maya")
+    
+#numb1=14
+#print (numb1)
+#numb2 = numb1/2
+#print (numb2)
+
+#mylist=[4 , 6, 8]
+#sum = 0
+#for i in range(3):
+    #print(mylist[i]*2)
+ #   print (mylist[i])
+  #  sum = sum + mylist[i]
+#print(sum)
+    
+#import turtle
+#turtle.penup()
+#turtle.goto(100,100)
+#turtle.mainloop
+'''
+turtle.pensize(9)
+turtle.color("green")
+turtle.circle(70)
+turtle.color("yellow")
+turtle.penup()
+turtle.goto(-120,0)
+turtle.pendown()
+turtle.circle(70)
+turtle.color("red")
+turtle.penup()
+turtle.goto(60,80)
+turtle.pendown()
+turtle.circle(70)
+turtle.color("black")
+turtle.penup()
+turtle.goto(-60,80)
+turtle.pendown()
+turtle.circle(70)
+turtle.color("blue")
+tuetlr.penup()
+turtle.goto(180,80)
+turtle.pendown()
+'''
+turtle.circle(70)
+
